5ca52af8292523be5031003c1729c53336154909
Samuel Ignacio Arango Ramírez <superajke@gmail.com>
2024-01-24 14:50:13 -0500

    Version 0.1
    Se crea el esqueleto del To Do

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "./components/sidebar";
+
+function App() {
+  return (
+    <section>
+      <Sidebar />
+    </section>
+  );
+}
+
+export default App;
---
+++ src/mocks/categorias.js
@@ -0,0 +1,38 @@
+export const data =  [
+    {
+      id: 1,
+      nombre: "Trabajo",
+      fechaCreacion: "2024-01-24",
+      fechaModificacion: "2024-01-24",
+      todos: [
+        {
+          id: 1,
+          tarea: "Hacer informe",
+          completada: false,
+        },
+        {
+          id: 2,
+          tarea: "Enviar correo electrónico",
+          completada: true,
+        },
+      ],
+    },
+    {
+      id: 2,
+      nombre: "Personal",
+      fechaCreacion: "2024-01-24",
+      fechaModificacion: "2024-01-24",
+      todos: [
+        {
+          id: 1,
+          tarea: "Hacer ejercicio",
+          completada: false,
+        },
+        {
+          id: 2,
+          tarea: "Leer un libro",
+          completada: false,
+        },
+      ],
+    },
+  ]
